c4086b243af8f5779a5d19f82b5c652cb7284a67
Endergreen12 <endergreenjapan@gmail.com>
2024-08-07 15:57:58 +0900

    TextAssetのエクスポートされたファイルの拡張子を修正 Fontをエクスポートの対象から削除 コードを最適化

--- Blue-Archive-AssetBundle-Extractor/Blue_Archive_AssetBundle_Extractor.py
+++ Blue-Archive-AssetBundle-Extractor/Blue_Archive_AssetBundle_Extractor.py
@@ -1,4 +1,3 @@
-from tkinter.font import Font
 import UnityPy
 import glob
 import os
@@ -8,6 +7,8 @@ from UnityPy.classes import AudioClip, Mesh, TextAsset
 required_unitypy_version = '1.10.14'
 if UnityPy.__version__ != required_unitypy_version:
     raise ImportError(f"Invalid UnityPy version detected. Please use version {required_unitypy_version}")
+
+export_target_type_array = ["Texture2D", "Sprite", "AudioClip", "Mesh", "TextAsset"]
 
 path = input("Enter the path to AssetBundls: ").replace("\"", "")
 output_path = input("Enter the path to output: ").replace("\"", "")
@@ -19,47 +20,30 @@ for bundle_path in glob.glob(os.path.join(path, "*")):
     
     for path, obj in env.container.items():
         try:
-            path_array = path.split("/")
-            full_path_str = os.path.join(output_path, *path_array)
-            full_path_without_filename_str = os.path.join(output_path, *path_array[:-1]) # path_array[:-1]でファイル名を除く
-            os.makedirs(full_path_without_filename_str, exist_ok=True) # フォルダを作る
+            if not obj.type.name in export_target_type_array:
+                continue
             
+            full_path_dir_str = os.path.join(output_path, *path.split("/")[:-1]) # ファイル名を除いたフルパス
+            os.makedirs(full_path_dir_str, exist_ok=True) # フォルダを作る
+            
+            data = obj.read()
             match obj.type.name:
                 case "Texture2D" | "Sprite":
-                    data = obj.read()
-                    data.image.save(os.path.splitext(full_path_str)[0] + ".png") # 拡張子がPSDだと例外が出るのでsplitextで拡張子をのぞいたパスに.pngをくっつけて保存
+                    data.image.save(os.path.join(full_path_dir_str, data.name + ".png")) # 拡張子がPSDだと例外が出るのでsplitextで拡張子をのぞいたパスに.pngをくっつけて保存
                     
                 case "AudioClip":
-                    clip : AudioClip = obj.read()
-                    for name, data in clip.samples.items():
-                        with open(os.path.join(full_path_without_filename_str, name), "wb") as f:
-                            f.write(data)
-                            
-                case "Font":
-                    font : Font = obj.read()
-                    
-                    extension = ""
-                    
-                    if font.m_FontData:
-                        extension = ".ttf"
-                        
-                    if font.m_FontData[0:4] == b"OTTO":
-                        extension = ".otf"
-
-                    with open(os.path.join(full_path_without_filename_str, font.name + extension), "wb") as f:
-                        f.write(font.m_FontData)
+                    for name, audio_data in data.samples.items():
+                        with open(os.path.join(full_path_dir_str, name), "wb") as f:
+                            f.write(audio_data)
                         
                 case "Mesh":
-                    mesh : Mesh = obj.read()
-                    with open(os.path.join(full_path_without_filename_str, f"{mesh.name}.obj"), "wt", newline = "") as f:
+                    with open(os.path.join(full_path_dir_str, f"{data.name}.obj"), "wt", newline = "") as f:
                         # newline = "" is important
-                        f.write(mesh.export())
+                        f.write(data.export())
                         
                 case "TextAsset":
-                    data : TextAsset = obj.read()
-                    with open(full_path_str, "wb") as f:
+                    with open(os.path.join(full_path_dir_str, data.name), "wb") as f:
                         f.write(bytes(data.script))
-
                     
         except Exception as e:
             print(f"Exception occurred on {env.file.name}:")
